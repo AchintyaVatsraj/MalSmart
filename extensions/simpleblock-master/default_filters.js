55faf504aa09411952bdb38698d36ed23c312eb0
keerthanchrist <88071939+keerthanchrist@users.noreply.github.com>
2021-11-25 08:47:30 +0530

    Add files via upload

---
+++ extensions/simpleblock-master/default_filters.js
@@ -0,0 +1,16 @@
+defaultFilters = [
+	// personally, I can't stand the like box
+	//"http://www.facebook.com/plugins/likebox.php?*",
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+	"*://*.yt118.com/*",	
+
+]
